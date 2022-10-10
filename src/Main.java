6c5a481a4050853b2a4e10fc230ebde8a404ba8b
Anton <fynjycedjhjd@gmail.com>
2022-10-10 20:59:32 +0300

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ public class Main {
         //часть 1
         int[] massiv = new int[3];
         massiv[0] = 150;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 2; i >= 0; i--) {
             System.out.println(massiv[i]);
 
         }
         // часть 2
         double[] drobi = new double[]{1.57, 7.654, 9.986};
-        for (int w = 0; w < 3; w++) {
+        for (int w = 2; w >= 0; w--) {
             System.out.println(drobi[w]);
         }
 
         // часть 3
         boolean[] falseAndTrue = new boolean[]{false, true, 5>7};
-        for (int e = 0; e < 3; e++) {
+        for (int e = 2; e >= 0; e--) {
             System.out.println(falseAndTrue[e]);
         }
 
